69f3a3f82733279dac3e7c00a50b795e538a49d9
priyanandini1507 <84395860+priyanandini1507@users.noreply.github.com>
2023-02-25 11:24:01 +0530

    Create prob1_batch2.py

---
+++ prob1_batch2.py
@@ -0,0 +1,10 @@
+print("Please enter in this format if its 2/3 enter x=2  y=3")
+x=int(input("enter x:"))
+y=int(input("enter y:"))
+m=int(input("enter total income:"))
+print("If u want to find out 1/4th part Please enter in this format p=1  j=4")
+p=int(input("enter p:"))
+j=int(input("enter j:"))
+wi=(m)*(y/x)
+
+print(int(int(wi)*(p/j)))
