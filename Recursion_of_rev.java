9df34bea63aced4693965d492983504a6b1fbdf8
priyanandini1507 <84395860+priyanandini1507@users.noreply.github.com>
2023-03-02 19:46:12 +0530

    Create Recursion_of_rev.java

---
+++ Recursion_of_rev.java
@@ -0,0 +1,17 @@
+public class Recursion_of_rev {
+     public static void printReverse (String str)
+        {
+                if ((str==null)||(str.length() <= 1)) 
+                   System.out.print(str); 
+                else
+                { 
+                    System.out.print(str.charAt(str.length()-1));             
+                    printReverse(str.substring(0,str.length()-1)); 
+                }    
+        }
+    public static void main(String[] args) {
+        String str="this function reverse";
+        printReverse(str);
+    }
+
+}
